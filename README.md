edccf1b897ddf6ab4f513794b81700bcec652f66
Võ Tấn Thành <votanthanh32004@gmail.com>
2026-07-10 14:07:26 +0700

    feat: implement authentication UI components and add UploadUrlRequest DTO

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blog-website
 A beautiful blog website help you bring you knowledge to everyone
+
+## Live Demo
+- Frontend: [https://blog-web-two-bay.vercel.app/](https://blog-web-two-bay.vercel.app/)
--- be/src/main/java/com/blog/be/storage/api/dto/UploadUrlRequest.java
+++ be/src/main/java/com/blog/be/storage/api/dto/UploadUrlRequest.java
@@ -1,11 +1,20 @@
 package com.blog.be.storage.api.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UploadUrlRequest {
+    @NotBlank(message = "Tên file không được để trống")
     private String fileName;
+    
+    @NotBlank(message = "Loại file (contentType) không được để trống")
     private String contentType;
+    
+    @NotBlank(message = "Thư mục không được để trống")
     private String folder;
+    
+    @Min(value = 1, message = "Kích thước file phải lớn hơn 0")
     private long fileSize;
 }
