a80992998adb571b3f0d64329d2afe8dde5be46c
Võ Tấn Thành <votanthanh32004@gmail.com>
2026-07-18 13:55:44 +0700

    feat: implement admin layout and authentication page templates

--- frontend/src/app/layout/admin-layout/admin-layout.component.ts
+++ frontend/src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.scss'
+  styleUrl: './admin-layout.component.css'
 })
 export class AdminLayoutComponent {
 
